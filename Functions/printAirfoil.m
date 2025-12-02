c0e418f5a4577fe8f223d22f13a4b32819ea448e
ccardo <121583980+ccardo@users.noreply.github.com>
2025-12-02 21:36:44 +0100

    Add files via upload

---
+++ Functions/printAirfoil.m
@@ -0,0 +1,30 @@
+function [] = printAirfoil(T, B)
+    
+    x = (1 - cos(linspace(0, pi)))/2;
+    [~, yT] = CSTcurve(x, T);
+    [~, yB] = CSTcurve(x, B);
+
+    x = x(:);
+    yT = yT(:);
+    yB = yB(:);
+
+    coords = [x(end:-1:2) yT(end:-1:2)
+              x           yB];
+    
+    % check current directory to EMWET
+    directory = dir();
+    if contains(directory(1).folder, "Assignment")
+        cd .\EMWET\
+    elseif contains(directory(1).folder, "EMWET")
+        cd .\
+    else
+        cd ..\EMWET\
+    end
+
+    fid = fopen("current_airfoil.txt", "w");
+    fprintf(fid, "%.4f %.4f\n", coords');
+    fclose(fid);
+
+    cd ..\
+
+end
