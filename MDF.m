9f0ddf31e8242949a7c1a36b8d5e7ab67aafa4d2
ccardo <riccardo.gallo.pb+github@gmail.com>
2025-12-03 10:23:43 +0100

    updated lower and upper bounds; added descriptions to design variables

--- MDF.m
+++ MDF.m
@@ -1,36 +1,74 @@
 clc
-clear all
+clear
 close all
 
 run init_FixedValues.m
 
 % Initial values
 
-Ma_des = 0.82; % mach number 
-h_des = 11800; % altitude [m]
-c_kink = 7.514; % chord at the kink [m]
-c_tip = 2.312; % chord at the tip [m]
-T1 = 0.14863;
-T2 = 0.069323;
-T3 = 0.22575;
-T4 = 0.040425;
-T5 = 0.27305;
-T6 = 0.17076;
-T7 = 0.27171;
-B1 = -0.015853;
-B2 = -0.082473;
-B3 = -0.16792;
-B4 = -0.038631; 
-B5 = -0.26127;
-B6 = 0.075531;
-B7 = 0.077234;
-LE_sweep = 32; % leading edge sweep [deg]
-b2 = 20.81; % outer span [m]
+Ma_des = 0.82;          % mach number 
+h_des = 11800;          % altitude [m]
+c_kink = 7.514;         % chord at the kink [m]
+c_tip = 2.312;          % chord at the tip [m]
+T1 = 0.14863;           % \
+T2 = 0.069323;          % |
+T3 = 0.22575;           % |
+T4 = 0.040425;          % } top CST coefficients
+T5 = 0.27305;           % |
+T6 = 0.17076;           % |
+T7 = 0.27171;           % /
+B1 = -0.015853;         % \
+B2 = -0.082473;         % |
+B3 = -0.16792;          % |
+B4 = -0.038631;         % } bottom CST coefficients
+B5 = -0.26127;          % |
+B6 = 0.075531;          % |
+B7 = 0.077234;          % /
+LE_sweep = 32;          % leading edge sweep [deg]
+b2 = 20.81;             % outer span [m]
 
-%bounds
+% bounds
+lb = [0.9 * FixedValues.Performance.Ma_des_ref
+      0.9 * FixedValues.Performance.h_des_ref
+      1
+      0.5
+      -Inf
+      -Inf
+      -Inf
+      -Inf
+      -Inf
+      -Inf
+      -Inf
+      -Inf
+      -Inf
+      -Inf
+      -Inf
+      -Inf
+      -Inf
+      -Inf
+      10
+      0];
 
-lb = [0.9 * FixedValues.Performance.M_ref_des, 0.9 * FixedValues.Performance.h_red_des];
-ub = [1.1 * FixedValues.Performance.M_ref_des, 1.1 * FixedValues.Performance.h_ref_des];
+ub = [1.1 * FixedValues.Performance.Ma_des_ref
+      1.1 * FixedValues.Performance.h_des_ref
+      15
+      Inf
+      Inf
+      Inf
+      Inf
+      Inf
+      Inf
+      Inf
+      Inf
+      Inf
+      Inf
+      Inf
+      Inf
+      Inf
+      Inf
+      Inf
+      50
+      25];
 
 
 % design vector
