4d2ce8ec471f73dd3318b1b72d4d99cbefbdcaf3
ccardo <riccardo.gallo.pb+github@gmail.com>
2026-01-03 16:38:47 +0100

    changed directory switching in Aerodynamics. Changed finite difference step size to 5e-3.

--- Disciplines/Aerodynamics.m
+++ Disciplines/Aerodynamics.m
@@ -35,80 +35,72 @@ function [L_des, D_des, D_des_wing, alpha] = Aerodynamics(Aircraft, W_wing, v)
 
     Aircraft.Visc = 1;
 
-    % check current directory and change to Q3D
-    % result = changeDirSafe("Q3D");
-    result = 1;
-
-    if result
         
-        lastwarn("")
-        warning("off", "backtrace")
+    lastwarn("")
+    warning("off", "backtrace")
 
-        % set a 120-second timer for Aero to complete, else mark it as an 
-        % error. Do this using a parallel worker so it can kill the process
-        %  if the solver times out. Same as for the MDA.
-        
-        % create a new background pool (if there is none)
-        pool = gcp('nocreate');
-        if isempty(pool)
-            pool = parpool(1);
-        end
-        
-        disp("[AER] Running Q3D...")
-
-        % run Q3D in parallel (2 expected outputs)
-        Aero = parfeval(pool, @AeroEval, 2, ...
-            Aircraft);
-
-        startingTime = tic;
-        while toc(startingTime) < 120
-            % if finishes early, continue without a problem
-            if Aero.State == "finished"
-                break
-            end
-        end
-
-        % if function is still running after 120 seconds, warning + error
-        if Aero.State ~= "finished"
-            cancelAll(pool.FevalQueue)
-            cancel(Aero)
-            warning on
-            warning("off", "backtrace")
-            warning("off", "verbose")
-            warning("Q3D [AER] has been running for more than 120 seconds.");
-            warning("on", "backtrace")
-            
-            parfeval(pool, @changeWorkerDir, 0, projectDirectory);
-
-            error("Q3D [AER] has been running for more than 120 seconds.")
-        end
-        
-        % if q3d outputs some errors for some reason then boom
-        if ~isempty(Aero.Error)
-            exc = Aero.Error.remotecause{:};
-            warning on
-            warning("off", "backtrace")
-            warning("off", "verbose")
-            warning("Q3D [AER] has produced an unexpected error");
-            warning("on", "backtrace")
-            throw(exc)
-        end
-
-        % catch ALL warnings by q3d, catch error by outer block
-        [msg, ~] = lastwarn();
-        if contains(msg, "airfoil transonic analysis diverged")
-            error("Q3D did not converge.")
-        end
-
-        % get results from Q3D
-        Res = Aero.OutputArguments{1};
-        finish = Aero.OutputArguments{2};
-
-        disp("[AER] Time elapsed: " + finish)
-        % cd ..\
-    else
-        error("ERROR: could not change directory to Q3D from Aerodynamics")
+    % set a 120-second timer for Aero to complete, else mark it as an 
+    % error. Do this using a parallel worker so it can kill the process
+    %  if the solver times out. Same as for the MDA.
+    
+    % create a new background pool (if there is none)
+    pool = gcp('nocreate');
+    if isempty(pool)
+        pool = parpool(1);
     end
+    
+    disp("[AER] Running Q3D...")
+   
+    run Q3D in parallel (2 expected outputs)
+    Aero = parfeval(pool, @AeroEval, 2, ...
+        Aircraft);
+
+    startingTime = tic;
+    while toc(startingTime) < 120
+        % if finishes early, continue without a problem
+        if Aero.State == "finished"
+            break
+        end
+    end
+
+    % if function is still running after 120 seconds, warning + error
+    if Aero.State ~= "finished"
+        cancelAll(pool.FevalQueue)
+        cancel(Aero)
+        warning on
+        warning("off", "backtrace")
+        warning("off", "verbose")
+        warning("Q3D [AER] has been running for more than 120 seconds.");
+        warning("on", "backtrace")
+
+        parfeval(pool, @changeWorkerDir, 0, projectDirectory);
+
+        error("Q3D [AER] has been running for more than 120 seconds.")
+    end
+
+    % if q3d outputs some errors for some reason then boom
+    if ~isempty(Aero.Error)
+        exc = Aero.Error.remotecause{:};
+        warning on
+        warning("off", "backtrace")
+        warning("off", "verbose")
+        warning("Q3D [AER] has produced an unexpected error");
+        warning("on", "backtrace")
+        throw(exc)
+    end
+
+    % catch ALL warnings by q3d, catch error by outer block
+    [msg, ~] = lastwarn();
+    if contains(msg, "airfoil transonic analysis diverged")
+        error("Q3D did not converge.")
+    end
+
+    % get results from Q3D
+    Res = Aero.OutputArguments{1};
+    finish = Aero.OutputArguments{2};
+
+    disp("[AER] Time elapsed: " + finish)
+    % cd ..\
     
     % if D is NaN, sqp will handle it.
     alpha = Res.Alpha;
