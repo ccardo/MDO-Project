dcac54d48efaaf2efcda9957b09493d3fde9509a
ccardo <riccardo.gallo.pb+github@gmail.com>
2026-01-04 11:51:25 +0100

    added isothermal density change in stratosphere

--- Functions/airDensity.m
+++ Functions/airDensity.m
@@ -1,5 +1,7 @@
 function rho = airDensity(h)
 
-    rho = 1.225 * (airTemperature(h)/288.15) ^ (5.2561-1);
+          % adiabatic troposphere, isothermal stratosphere
+    rho = 1.225 * (airTemperature(h)/288.15) .^ (5.2561-1) .* (h <= 11000) + ...
+        0.3639 * exp(-157.69e-6 * (h - 11000)) .* (h > 11000);
 
 end
