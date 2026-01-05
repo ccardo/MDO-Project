1e1657439396a807424758d5e212c077062dd902
ccardo <riccardo.gallo.pb+github@gmail.com>
2026-01-05 22:10:34 +0100

    changed some thingies

--- Functions/airDensity.m
+++ Functions/airDensity.m
@@ -2,6 +2,6 @@ function rho = airDensity(h)
 
           % adiabatic troposphere, isothermal stratosphere
     rho = 1.225 * (airTemperature(h)/288.15) .^ (5.2561-1) .* (h <= 11000) + ...
-        0.3639 * exp(-157.69e-6 * (h - 11000)) .* (h > 11000);
+         0.3639 * exp(-157.69e-6 * (h - 11000)) .* (h > 11000);
 
 end
