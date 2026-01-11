3b357332f34fba9be247071a4698b1fd4c531b2b
ronchettiandrea2003-afk <ronchetti.andrea.2003@gmail.com>
2026-01-11 18:29:23 +0100

    added function to plot res

--- main.m
+++ main.m
@@ -73,7 +73,7 @@ lb = [0.9 * FixedValues.Performance.Ma_des_ref          % Ma_des
    -0.3000                                              % B2
    -0.3000                                              % B3
    -0.3000                                              % B4
-   -0.5000                                              % B5 RELAXED
+   -0.5000                                              % B5
    -0.3000                                              % B6
    -0.3000                                              % B7
       10                                                % LE_sweep
@@ -88,9 +88,9 @@ ub = [FixedValues.Performance.Ma_MO                     % Ma_des
     0.3000                                              % T2
     0.3000                                              % T3
     0.3000                                              % T4
-    0.5000                                              % T5 RELAXED
+    0.5000                                              % T5 
     0.3000                                              % T6
-    0.5000                                              % T7 RELAXED
+    0.5000                                              % T7 
    -0.0500                                              % B1
    -0.0500                                              % B2
     0.0100                                              % B3
@@ -136,7 +136,7 @@ options = optimoptions('fmincon');
 options.Display                     = 'iter-detailed';
 options.Algorithm                   = 'sqp';
 options.FunValCheck                 = 'off';        % When turned on displays an error when the objective function or constraints return a value that is complex, NaN, or Inf. By turning it off, fmincon can handle NaN values
-options.MaxIter                     = 1000;          % Maximum number of iterations
+options.MaxIter                     = 1000;         % Maximum number of iterations
 options.ScaleProblem                = true;         % Normalization of the variables
 options.PlotFcn                     = {@optimplotfval, @optimplotx, @optimplotfirstorderopt, @optimplotstepsize, @optimplotconstrviolation, @optimplotfunccount};
 options.FiniteDifferenceType        = 'forward';
@@ -152,21 +152,6 @@ options.OutputFcn                   = {@outConst, @outFun, @outWWing, @outInform
 optimStart = tic;
 [x,FVAL,EXITFLAG,OUTPUT] = fmincon(@Optimizer, v0, [], [], [], [], lb, ub, @constraints, options);
 optimEnd = toc(optimStart);
-
-% Plot of the convergence history of the objective function 
-figure('Name', 'Obj function', 'NumberTitle', 'off')
-iterCount = size(c_hist, 1)-1;
-plot(0:iterCount, f_hist, 'k.-', "MarkerSize", 25, "LineWidth",2)
-axis tight
-ylim([min(f_hist)-0.01, max(f_hist)+0.01])
-title("Convergence history of the objective function")
-xlabel("Iteration")
-ylabel("Objective function")
-grid minor
-
-% Plots of the convergence history of the constraint functions
-
-plotConstraints(c_hist, iterCount)
 
 fprintf("Current directory: %s", cd)
 fprintf("Current project directory: %s", projectDirectory)
@@ -210,217 +195,17 @@ FVAL = FVAL * FixedValues.Performance.R_ref;
 final_V = normalize(x, "denorm", FixedValues.Key.designVector);
 
 % display all of the optimization results
-% dispRes(final_V, FVAL, c1(end), c2(end), W_wing_hist(end))
+dispRes(final_V, FVAL, c1(end), c2(end), W_wing_hist(end))
 
-figNumbers = randi(1e9, 6, 1);
+% plot 
+plotRes(c_hist, f_hist, Final_V)
 
-% plot airfoil geometry
-Ti = final_V(5:11);
-Bi = final_V(12:18);
-chord = 1 - cos(linspace(0, pi/2));
-[~, yt] = CSTcurve(chord, Ti);
-[~, yb] = CSTcurve(chord, Bi);
-thickness = yt - yb;
-camber = 1/2 * (yt + yb);
-Ti_ref = FixedValues.Reference_Aircraft.Wing.Airfoils(1, 1:7);
-Bi_ref = FixedValues.Reference_Aircraft.Wing.Airfoils(1, 8:end);
-[~, yt_ref] = CSTcurve(chord, Ti_ref);
-[~, yb_ref] = CSTcurve(chord, Bi_ref);
-
-gcf = figure(figNumbers(1));
-gcf.Name = "Airfoils Equal Axes";
-line(chord, yt, "Color", "k", "LineWidth", 1)
-line(chord, yb, "Color", "k", "LineWidth", 1)
-line(chord, yt_ref, "Color", "r", "LineStyle", "--", "LineWidth", 1)
-line(chord, yb_ref, "Color", "r", "LineStyle", "--", "LineWidth", 1)
-L = legend("Current Airfoil", "", "Reference Airfoil");
-L.Location = "best";
-L.FontSize = 15;
-title("Final Airfoil", "FontSize", 15);
-T = title(gcf.Name);
-T.FontSize = 20;
-T.FontName = "Times New Roman";
-ylabel("y/c", FontSize=15)
-xlabel("x/c", FontSize=15)
-axis equal
-
-gcf = figure(figNumbers(2));
-gcf.Name = "Airfoils Scaled Axes";
-line(chord, yt, "Color", "k", "LineWidth", 1)
-line(chord, yb, "Color", "k", "LineWidth", 1)
-line(chord, yt_ref, "Color", "r", "LineStyle", "--", "LineWidth", 1)
-line(chord, yb_ref, "Color", "r", "LineStyle", "--", "LineWidth", 1)
-L = legend("Current Airfoil", "", "Reference Airfoil");
-L.Location = "best";
-L.FontSize = 15;
-title("Final Airfoil", "FontSize", 15);
-T = title(gcf.Name);
-T.FontSize = 20;
-T.FontName = "Times New Roman";
-ylabel("y/c", FontSize=15)
-xlabel("x/c", FontSize=15)
-axis normal
-
-% plot wing planforms with and without tanks
-
-gcf = figure(figNumbers(3));
-gcf.Name = "Wing Planforms";
-styleRef = {"r--", "LineWidth", 1};
-styleNew = {"Color", [0.4 0.4 0.7 1], "LineWidth", 1};
-plotWingGeometry(FixedValues.Reference_Aircraft.Wing.Geom, FixedValues.Reference_Aircraft.Wing.Airfoils, styleRef)
-hold on
-final_AC = createGeom(final_V);
-plotWingGeometry(final_AC.Wing.Geom, final_AC.Wing.Airfoils, styleNew);
-hold on
-% plot fuel tank in the same subfigure
-Boxes = loftWingBox(FixedValues.Reference_Aircraft, 20, 20, 0);
-for i = 1:length(Boxes)
-
-    surf(Boxes(i).X, Boxes(i).Y, Boxes(i).Z, ...
-         'FaceColor', [1 0.8 0.8], ...
-         'FaceAlpha', 0.5, ...
-         'EdgeColor', 'none', ...
-         "FaceLighting", "flat");
-end
-Boxes = loftWingBox(final_AC, 20, 20, 0);
-for i = 1:length(Boxes)
-
-    surf(Boxes(i).X, -Boxes(i).Y, Boxes(i).Z, ...
-         'FaceColor', [0.5 0.5 1], ...
-         'FaceAlpha', 0.3, ...
-         'EdgeColor', 'none', ...
-         "FaceLighting", "flat");
-end
-view(90, 90)
-L = legend("Reference Wing", "", "", "", "", "", "", "", "", "", "Final Wing");
-L.Position = [0.77 0.59 0.1 0.05];
-T = title(gcf.Name);
-T.FontSize = 20;
-T.FontName = "Times New Roman";
-xlabel X
-ylabel Y
-zlabel Z
-hold off
-axis padded
-
-
-% plotting 3D wings
-% little exploit, pretend that the tank is actually the whole wing to reuse
-% the old loftWingBox function
-FixedValues.Geometry.tank = [0 1];
-FixedValues.Geometry.spars = [0 1; 0 1; 0 1];
-
-gcf = figure(figNumbers(4));
-gcf.Name = "Reference Wing Isometric View";
-reference_wing3D = loftWingBox(FixedValues.Reference_Aircraft, 10, 20);
-for i = 1:length(Boxes)
-
-    surf(reference_wing3D(i).X, reference_wing3D(i).Y, reference_wing3D(i).Z, ...
-        'FaceColor', [1 0.7 0.7], ...
-        "FaceAlpha", 0.9, ...
-         'EdgeColor', 'k', ...
-         "EdgeAlpha", 0.3, ...
-         "FaceLighting", "gouraud");
-    hold on
-    surf(reference_wing3D(i).X, -reference_wing3D(i).Y, reference_wing3D(i).Z, ...
-        'FaceColor', [1 0.7 0.7], ...
-         'EdgeColor', 'k', ...
-         "EdgeAlpha", 0.3, ...
-         "FaceLighting", "gouraud");
-end
-grid off
-T = title(gcf.Name);
-T.FontSize = 20;
-T.FontName = "Times New Roman";
-xlabel X
-ylabel Y
-zlabel Z
-light
-material shiny
-axis equal
-axis padded
-
-gcf = figure(figNumbers(5));
-gcf.Name = "Final Wing Isometric View";
-final_wing3D = loftWingBox(final_AC, 10, 20);
-for i = 1:length(Boxes)
-
-    surf(final_wing3D(i).X, final_wing3D(i).Y, final_wing3D(i).Z, ...
-        'FaceColor', [0.7 0.7 1], ...
-         'EdgeColor', 'k', ...
-         "EdgeAlpha", 0.3, ...
-         "FaceLighting", "gouraud");
-    hold on
-    surf(final_wing3D(i).X, -final_wing3D(i).Y, final_wing3D(i).Z, ...
-        'FaceColor', [0.7 0.7 1], ...
-         'EdgeColor', 'k', ...
-         "EdgeAlpha", 0.3, ...
-         "FaceLighting", "gouraud");
-end
-grid off
-T = title(gcf.Name);
-T.FontSize = 20;
-T.FontName = "Times New Roman";
-xlabel X
-ylabel Y
-zlabel Z
-light
-axis equal
-axis padded
-
-
-gcf = figure(figNumbers(6));
-gcf.Name = "Overlapped Wings Isometric View";
-for i = 1:length(Boxes)
-
-    % reference
-    surf(reference_wing3D(i).X, reference_wing3D(i).Y, reference_wing3D(i).Z, ...
-         'FaceColor', [1 0.7 0.7], ...
-         "FaceAlpha", 0.5, ...
-         'EdgeColor', 'k', ...
-         "EdgeAlpha", 0.1, ...
-         "FaceLighting", "flat");
-    hold on
-    surf(reference_wing3D(i).X, -reference_wing3D(i).Y, reference_wing3D(i).Z, ...
-         'FaceColor', [1 0.7 0.7], ...
-         "FaceAlpha", 0.5, ...
-         'EdgeColor', 'k', ...
-         "EdgeAlpha", 0.1, ...
-         "FaceLighting", "flat");
-    
-    % final
-    surf(final_wing3D(i).X, final_wing3D(i).Y, final_wing3D(i).Z, ...
-         'FaceColor', [0.7 0.7 1], ...
-         "FaceAlpha", 0.7, ...
-         'EdgeColor', 'k', ...
-         "EdgeAlpha", 0.1, ...
-         "FaceLighting", "gouraud");
-    surf(final_wing3D(i).X, -final_wing3D(i).Y, final_wing3D(i).Z, ...
-         'FaceColor', [0.7 0.7 1], ...
-         "FaceAlpha", 0.7, ...
-         'EdgeColor', 'k', ...
-         "EdgeAlpha", 0.1, ...
-         "FaceLighting", "gouraud");
-end
-grid off
-L = legend("Reference Wing", "", "Final Wing");
-L.Position = [0.67 0.64 0.15 0.07];
-T = title(gcf.Name);
-T.FontSize = 20;
-T.FontName = "Times New Roman";
-xlabel X
-ylabel Y
-zlabel Z
-light
-axis equal
-axis padded
 
 % save the plots in the same results folder
 cd Results\
 figuresDir = sprintf("%s\\figures", subDirName);
 mkdir(figuresDir)
 cd(figuresDir)
-figList = [figNumbers(:); 11; 12; 1];
 formats = ["pdf", "png", "fig", "svg"];
 saveFigures("all", formats);
 cd ..\..\
